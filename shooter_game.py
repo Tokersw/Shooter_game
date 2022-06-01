c0625d04de6bf9023bb1e9f1c1573a5c7b124a25
Tokersw <91149339+Tokersw@users.noreply.github.com>
2022-06-01 23:40:25 +0800

    Update shooter_game.py
    V4 
    add menu (reset game, exit)

--- shooter_game.py
+++ shooter_game.py
@@ -1,150 +1,114 @@
-#Создай собственный Шутер!
-
 from pygame import *
-from random import randint
-from time import time as timer
-mixer.init()
 font.init()
-
-missed = 0
-score = 0 
-num_fire = 0
-rel_time = False
-font = font.SysFont("Arial", 40)
-
-score_text = font.render('Счет: '+ str(score), True, (255, 255, 255))
-missed_text = font.render('Пропущено: '+ str(missed), True, (255, 255, 255))
-lose = font.render('You lose', True, (255, 0, 0))
-win = font.render('You won', True, (255, 0, 0))
-reload_time = font.render('Waid, reload...', True, (255, 255, 255))
-
-fire = mixer.Sound("fire.ogg")
-mixer.music.load("space.ogg")
-mixer.music.play()
-w, h = 700, 500
-window = display.set_mode((w, h))
-display.set_caption("pygame window")
-background = transform.scale(image.load("galaxy.jpg"), (w,h))
-clock = time.Clock()
-
-finish = False
-
+#adding classes
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_speed, player_x, player_y, player_image, w=65, h=65):
+    def __init__(self, player_speed, player_x, player_y, player_image, w=50, h=50):
         super().__init__() 
         self.speed = player_speed
         self.image = transform.scale(image.load(player_image), (w, h))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.start_position = (325, 225)
+    def reset_ball(self):
+        self.rect.center = self.start_position
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
-    def update(self):
-        key_pressed = key.get_pressed()
-        if key_pressed[K_RIGHT] and self.rect.x < 630:
-            self.rect.x += 10
-        elif key_pressed[K_LEFT] and self.rect.x > 10:
-            self.rect.x -= 10
-    def fire(self):
-        bullet = Bullet(5, self.rect.centerx, self.rect.top,"bullet.png", 15,20)
-        bullets.add(bullet)
         
-
-class Enemy(GameSprite):
-    def update(self):
-        global missed
-        self.rect.y += self.speed
-        if self.rect.y >= 500:
-            missed = missed + 1
-            self.rect.y = 0
-            self.rect.x = randint(0, 650)
-
-class Bullet(GameSprite):
-    def update(self):
-        self.rect.y -= self.speed
-        if self.rect.y <= 0:
-            self.kill()
-class Asteroid(GameSprite):
-    def update(self):
-        self.rect.y += self.speed
-        if self.rect.y >= 500:
-            self.rect.y = 0
-            self.rect.x = randint(0, 650)
+class Player(GameSprite):
+    def update_r(self):
+        key_pressed_o = key.get_pressed()
+        if key_pressed_o[K_UP] and self.rect.y > 5:
+            self.rect.y -= 10
+        elif key_pressed_o[K_DOWN] and self.rect.y <  345:
+            self.rect.y += 10
+    def update_l(self):
+        key_pressed_t = key.get_pressed()
+        if key_pressed_t[K_w] and self.rect.y > 5:
+            self.rect.y -= 10
+        elif key_pressed_t[K_s] and self.rect.y < 345:
+            self.rect.y += 10
+            
+def move_ball():
+    global speed_x, speed_y, finish
     
-hero = Player(10, 350, 420, "rocket.png")
-monsters = sprite.Group()
-bullets = sprite.Group()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+        
+    if ball.rect.y <= 0 or ball.rect.y >= 450:
+        speed_y *= -1
+    
+    if sprite.collide_rect(platform1, ball):
+        speed_x *= -1
+    if sprite.collide_rect(platform2, ball):
+        speed_x *= -1
+    
+#creating a window
+w, h = 700, 500
+window = display.set_mode((w, h))
+display.set_caption("Ping pong pix by Tok")
+clock = time.Clock()
+finish = False
 
-asteroids = sprite.Group()
+myfont = font.SysFont("Opel Sans", 35)
+win_b = myfont.render('Blue won', True, (255, 255, 255))
 
-for l in range(3):
-    asteroid = Asteroid(randint(1,2), randint(10, 640), 0,"asteroid.png")
-    asteroids.add(asteroid)
-for s in range(5):
-    enemy = Enemy(randint(1,2), randint(10, 640), 0, "ufo.png")
-    monsters.add(enemy)
+speed_x = 5
+speed_y = 5
+
+background = transform.scale(image.load("background1.png"), (w,h))
+finish_i = transform.scale(image.load("finish.png"), (700,500))
+black = transform.scale(image.load("black.png"), (700,500))
+red_won = transform.scale(image.load("red_won.png"), (700,500))
+blue_won = transform.scale(image.load("blue_won.png"), (700,500))
 
 
+#platform
+platform1 = Player(10, 50, 150, "platformd.png", 18, 125)
+platform2 = Player(10, 635, 150, "platformd2.png", 18, 125)
+ball = GameSprite(10, 325, 225, "ball.png", 50, 50)
+ 
 game = 1
+#game cycle
 while game:
+    window.blit(background, (0,0))
+    
     for e in event.get():
         if e.type == QUIT:
             game = 0
-        elif e.type== KEYDOWN:
-            if e.key == K_SPACE:
-                if num_fire < 5 and rel_time == False:
-                     hero.fire()
-                     fire.play()
-                     num_fire += 1
-                else:
-                    rel_time = True
-                    last_time = timer()
-
-
-    if finish != True:
+        if finish == True:
+            if e.type == MOUSEBUTTONDOWN:
+                s = mouse.get_pos()
+                if 171 <= s[0] <= 284 and 256 <= s[1] <= 284:
+                    ball.reset_ball()
+                    finish = False
+                elif 171 <= s[0] <= 341 and 236 <= s[1] <= 341:
+                    game = 0
+                    
+    if finish == False:
         
-
-
-        missed_text = font.render('Пропущено: '+ str(missed), True, (255, 255, 255))
-        score_text = font.render('Счет: '+ str(score), True, (255, 255, 255))
-
-        window.blit(background, (0,0))
-        window.blit(score_text, (5, 25))
+        platform1.reset()#show platform1
+        platform1.update_l()#move platform1
         
-        monsters.draw(window)
-        bullets.draw(window)
-        asteroids.draw(window)
+        platform2.reset()#show platform2
+        platform2.update_r()#move platform2
         
-        hero.reset()
-        hero.update()
-
-        bullets.update()
-        monsters.update()
-        asteroids.update()
+        ball.reset() #show ball
         
-        sprite_list = sprite.groupcollide(monsters, bullets, True, True)
-        for s in sprite_list:
-            score += 1
-            enemy = Enemy(randint(1,2), randint(10, 640), 0, "ufo.png")
-            monsters.add(enemy)
-
-        if score == 10:
+        move_ball() #move ball
+        
+        if ball.rect.x >= 650:
+            window.blit(black, (0,0))
+            window.blit(finish_i, (0, 0))
+            window.blit(red_won, (0, 0))
             finish = True
-            window.blit(win, (325, 225))
-        sprite_list_two = sprite.spritecollide(hero, monsters, True)
-        sprite_list_three = sprite.spritecollide(hero, asteroids, True)
-        if sprite_list_two or sprite_list_three or missed >= 13:
-            finish = True
-            window.blit(lose, (325, 225))
-        if rel_time == True:
-            new_time = timer()
-            if new_time - last_time >= 3:
-                num_fire = 0
-                rel_time = False
             
-            window.blit(reload_time, (300, 450))
-    window.blit(missed_text, (5, 75))
-    display.update()
-    clock.tick(60)
+        if ball.rect.x <= 0:
+            window.blit(black, (0,0))
+            window.blit(finish_i, (0, 0))
+            window.blit(blue_won, (0, 0))
+            finish = True
+        
+        display.update()   
+        clock.tick(60)
